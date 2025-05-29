64e0a40b9be7f5c597cad95deaa496949c903694
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2025-05-29 11:00:01 +0200

    update after last meeting

---
+++ src/main/java/org/teacommerce/domain/product/SteepingRules.java
@@ -0,0 +1,10 @@
+package org.teacommerce.domain.product;
+
+public class SteepingRules {
+    int time;
+    int temperature;
+    int waterVolume;
+    int quantity;
+
+
+}
--- src/main/java/org/teacommerce/domain/product/Tea.java
+++ src/main/java/org/teacommerce/domain/product/Tea.java
@@ -2,58 +2,41 @@ package org.teacommerce.domain.product;
 
 import java.util.List;
 
-public class Tea {
-    // getter for all: we might need the info later
-    private String brand; // Setter: no, a brand name is not modifiable
-    private Category category; //Setter: no, category names are strictly enumerated
-    private String name; //Setter: no, a tea name is not modifiable
-    private Origin origin; //Setter: no, tea origins are strictly enumerated
-    private List<String> flavours; // Setter: not necessary, as it is a list elements can be added or deleted withtou a setter,
-    // and it could be better to avoid the list to be fully replaced by a new one
-    private int oxydation; // Setter: no, the oxydation is roughly the same all the time
-    private int temperature; // Setter: no, the temperature is generally based on the category, and category is not modifiable
-    private int steepingTime; // Setter: yes, the steeping time could be modified depending on the harvest year for the tea...
-    private double price;  // Setter: yes, the price is fixed at first but can be modified by some events (e.g. extra taxes)
-    private int quantity; // Setter: yes, quantity can vary on various factors
+// TODO: se renseigner sur les identifiants uniques
+// TODO: créer un objet Price
+// TODO: qu'est-ce qu'oxydation ? qu'est-ce qu'un flavour?
+// TODO: kata
 
-    public Tea(String brand, Category category, String name, Origin origin, List<String> flavours, int oxydation, int temperature, int steepingTime, double price, int quantity) {
-        this.brand = brand;
-        this.category = category;
-        this.name = name;
-        this.origin = origin;
-        this.flavours = flavours;
-        this.oxydation = oxydation;
-        this.temperature = temperature;
-        this.steepingTime = steepingTime;
-        this.price = price;
-        this.quantity = quantity;
-    }
+public class Tea {
+
+    private String brand;
+    private Category category;
+    private String name; // serait bien d'avoir un identifiant unique
+    private Origin origin;
+    private List<String> flavours;
+    private int oxydation;
+    private SteepingRules steepingRules;
+    private double taxFreePricePerGram; // autre objet
 
     public boolean isFlavoured() {
         return !flavours.isEmpty();
     }
 
-    // Assuming that the price refers to 100g
-    public double totalPrice(int inputQuantity) {
-        return inputQuantity * (price / 100);
+    public double taxFreePriceBy(int quantity) {
+        return quantity * taxFreePricePerGram;
     }
 
-    public void infuse(int inputQuantity) {
-        System.out.print(name + ": Infuse during " + steepingTime + "min, with a water temperature of " + temperature + "degrees");
-    }
-
-    // Assuming that the steeping time refers to 3g
-    public boolean isInfused(int inputQuantity, int currentTime) {
-        int requiredTime = (steepingTime * inputQuantity) / 3;
-
-        if (currentTime >= requiredTime) {
-            return true;
-        } else {
-            int remainingTime = requiredTime - currentTime;
-            System.out.println("You must wait for " + remainingTime + " more minutes.");
-            return false;
-        }
-    }
+//    public boolean isInfused(int quantity, int currentTime) {
+//        int requiredTime = (steepingTime * quantity) / 3;
+//
+//        if (currentTime >= requiredTime) {
+//            return true;
+//        } else {
+//            int remainingTime = requiredTime - currentTime;
+//            System.out.println("You must wait for " + remainingTime + " more minutes.");
+//            return false;
+//        }
+//    }
 
 }
 
